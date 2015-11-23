a8c2c5c7af0c764cbd65da0dd52cef5d35b24470
Jumpei Ogawa <phanective@gmail.com>
2015-11-23 22:52:42 +0900

    Add missing -y in apt-get install

--- setup.sh
+++ setup.sh
@@ -69,7 +69,7 @@ npm install -g bower eslint geddy gulp
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 # Python build dependencies
-sudo apt-get install libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev
+sudo apt-get install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev
 
 if ! grep --fixed-strings --line-regexp "# PyEnv" ~/.bashrc; then
 cat << _EOF_ >> ~/.bashrc
