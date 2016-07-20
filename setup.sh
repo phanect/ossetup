61d3c3ce624653bddb171274b399679873316364
Jumpei Ogawa <phanect@users.noreply.github.com>
2016-07-20 19:28:17 +0900

    Allow non-root user to run Docker

--- setup.sh
+++ setup.sh
@@ -177,6 +177,11 @@ git config --global core.fileMode false
 git config --global push.default simple
 
 #
+# Allow non-root user to run Docker
+#
+sudo usermod --append --groups docker "$(whoami)"
+
+#
 # Aliases
 #
 if ! grep --fixed-strings --line-regexp "# colordiff" ~/.bashrc; then
