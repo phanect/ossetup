88de5443830b061841febb7f34eaa0a539e6ea89
Jumpei Ogawa <phanective@gmail.com>
2017-04-27 16:54:20 +0900

    Revert "Use NodeSource's apt repository to install Node.js"
    This reverts commit ce01bc31935799bdb02c76d68bcaacae169704e0.
    Some packages in KDE Neon depend on Ubuntu's nodejs package, so to avoid misbehavior, I will use nvm

--- setup.sh
+++ setup.sh
@@ -61,9 +61,6 @@ elif [ "$BASEDIST" = "debian" ]; then
   echo "deb http://mozilla.debian.net/ $CODENAME-backports firefox-release" | sudo tee /etc/apt/sources.list.d/firefox.list
 fi
 
-# Add Node.js Repo
-curl --silent --location https://deb.nodesource.com/setup_6.x | sudo --preserve-env bash -
-
 # Add yarn Repo
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -133,7 +130,25 @@ apm install atom-jinja2 \
 # Vagrant plugins
 vagrant plugin install vagrant-vbguest
 
-# NPMs
+#
+# Node.js Environment Setup
+#
+
+# Get latest version of NVM
+NVM_LATEST=$(curl --silent --show-error https://api.github.com/repos/creationix/nvm/releases/latest | jq --raw-output .name)
+
+touch ~/.bashrc
+curl --silent --show-error "https://raw.githubusercontent.com/creationix/nvm/$NVM_LATEST/install.sh" | bash
+
+# - .bashrc may occur error
+# - nvm doesn't work on strict mode
+set +eu
+. ~/.nvm/nvm.sh # .bashrc doesn't run without GUI on Ubuntu; Load nvm.sh directly
+nvm install 6
+set -eux
+
+nvm use 6
+nvm alias default 6
 npm update --global
 npm install --global bower eslint gulp
 
